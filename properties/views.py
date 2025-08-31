64b2fa3a13530f44eb9c2a6c180b24fee871befa
Allan <nabuleallan1@gmail.com>
2025-08-31 21:04:39 +0300

    added utils

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,13 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
+from django.http import JsonResponse
+from .utils import get_all_properties
+
+def property_list(request):
+    properties = get_all_properties()
+    return JsonResponse({"data": properties})
+
 
 @cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
